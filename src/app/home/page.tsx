4067259f99fa757d336d373772bbc049d554ae18
milena579 <milecalegari@gmail.com>
2024-12-05 11:05:33 -0300

    arrumando banner

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,18 +1,19 @@
 import { Card } from "@/componets/card/card";
 import { Footer } from "@/componets/footer/footer";
 import { Menu } from "@/componets/menu/menu";
-import { Carousel } from "@/componets/carousel/carousel";
 import Image from "next/image";
-
+import banner from "/public/banner.jpg";
 export default function Home() {
   return (
     <>
       {/* <Menu op1="" op2="" ></Menu> */}
-      <div className="flex bg-blue-100 py-6 px-14 items-cenetr justify-center">
+      <div className="flex bg-blue-100 py-6 px-14 z-1 items-cenetr justify-center">
         <h5>PRATA REAL</h5>
       </div>
-      <div className="h-[100vh]">
-        <Carousel></Carousel>
+      <div className="h-screen">
+        <div className="w-screen" >
+          <Image src={banner} alt="banner" className="object-cover w-screen h-96 absolute" priority></Image>
+        </div>
       </div>
       <Footer></Footer>
     </>
