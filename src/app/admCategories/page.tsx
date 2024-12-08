8c9322d9fbb2f1f81fccb7d88cf9d9ff4e362687
ingridcriistine <ingridrochacristine@gmail.com>
2024-12-08 19:26:49 -0300

    arrumando função de editar categoria

--- src/app/admCategories/page.tsx
+++ src/app/admCategories/page.tsx
@@ -18,13 +18,26 @@ export default function admCategories() {
     const [modalEditCategory, setModalEditCategory] = useState(false);
     const [dataCategories, setDataCategories] = useState<ICategory[]>([]);
     const [idCategory, setIdCategory] = useState<number>()
-
-    const [selectedCategory, setSelectedCategory] = useState<ICategory>();
     const [name, setName] = useState<string>("");
-
     const [editName, setEditName] = useState<string>("");
-
     const [error, setError] = useState<boolean>(false);
+    const [selectedCategory, setSelectedCategory] = useState<ICategory>();
+
+    useEffect(() => {
+        if (modalCategory) {
+            setModalEditCategory(false);
+        }
+    
+        if (modalEditCategory) {
+            setModalCategory(false);
+        }
+    }, [modalCategory, modalEditCategory]);
+
+    const [categoryData, setCategoryData] = useState<ICategory>({
+        id: 0,
+        name: ""
+    });
+
 
     const categories = async () => {
         var dataCategory: ICategory[] = []
@@ -36,18 +49,22 @@ export default function admCategories() {
             console.log(dataCategory);
 
         } catch (error) {
-            setDataCategories([{ "id": 0, "name": "ERRO AO CARREGAR CATEGORIAS"}]);
+            setDataCategories([{ "id": 0, "name": "ERRO AO CARREGAR CATEGORIAS" }]);
         }
     };
 
+    useEffect(() => {
+        categories();
+    }, []);
+
     const addCategory = async () => {
         try {
-            if(name == ""){
+            if (name == "") {
                 alert("Todos os campos devem ser preenchidos!")
                 return
             }
 
-            const response =  await fetch('http://localhost:8080/category', {
+            const response = await fetch('http://localhost:8080/category', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -58,29 +75,31 @@ export default function admCategories() {
                 }),
             });
 
-        
-            if(response.status === 500){
+
+            if (response.status === 500) {
                 setError(true);
-            }else{
+            } else {
                 alert("Categoria criada com sucesso!")
                 setError(false)
                 setModalCategory(false);
                 setName("");
                 categories();
             }
-            categories();
-        } 
-        
+        }
+
         catch (error) {
             setError(true)
         };
-
-        
     }
 
     const updateCategory = async () => {
+        if (!categoryData) {
+            alert("Nenhum dado encontrado para atualizar.");
+            return;
+        }
+
         try {
-            const response =  await fetch(`http://localhost:8080/category/?id=${idCategory}`, {
+            const response = await fetch(`http://localhost:8080/category/?id=${idCategory}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
@@ -91,33 +110,23 @@ export default function admCategories() {
                 }),
             });
 
-            if(name == ""){
-                alert("Todos os campos devem ser preenchidos!")
-                return
+            if (!response.ok) {
+                alert(`Erro ao atualizar categoria`);
+                setError(true);
+            } else {
+                alert("Categoria atualizada com sucesso!");
+                setError(false);
+                setModalEditCategory(false);
+                categories();
             }
-
-            const result = await response.json();
-            if(response.status === 500){
-                setError(true)
-            }else{
-                setError(false)
-                alert("Categoria criada com sucesso!")
-                setModalCategory(false);
-            }
-            console.log(result)
-        } 
-        
-        catch (error) {
-            setError(true)
-        };
+        } catch (error) {
+            console.error("Erro ao atualizar os dados de categoria:", error);
+            alert("Erro ao atualizar os dados.");
+            setError(true);
+        }
     }
 
-    useEffect(() => {
-        categories();
-    }, []);
 
-    const closeEditCategoryModal = () => setModalEditCategory(false);
-    
     const openEditCategoryModal = (data: ICategory) => {
         setSelectedCategory(data);
         setModalEditCategory(true);
@@ -132,7 +141,7 @@ export default function admCategories() {
                 </div>
 
                 <div className="mt-16 flex justify-center">
-                    <div className={modalCategory ? "mt-4 overflow-x-auto max-h-[350px] w-[80%] m-8" : "mt-4 overflow-x-auto max-h-[350px] w-[100%] m-48"}>
+                    <div className={(modalCategory || modalEditCategory) ? "mt-4 overflow-x-auto max-h-[350px] w-[80%] m-8" : "mt-4 overflow-x-auto max-h-[350px] w-[100%] m-48"}>
                         <table className="w-full bg-gray-50">
                             <thead className="w-full items-center flex-col flex ">
                                 <tr className="p-5 border-b-2 w-full flex justify-center">
@@ -145,34 +154,54 @@ export default function admCategories() {
                                     <th className="justify-center flex w-[25%] p-5">Deletar</th>
                                 </tr>
                             </thead>
-                                {dataCategories.map((item) => {
-                                    return (
-                                        <tbody className="overflow-y-auto">
-                                            <tr key={item.id} className="flex w-full h-14">
-                                                <td className="justify-center flex w-[25%] h-full border-b-2 p-2">{item.id}</td>
-                                                <td className="justify-start flex w-[25%] h-full border-b-2 p-2">{item.name}</td>
-                                                <td className="justify-center flex w-[25%] h-full border-b-2 p-2"><button onClick={() => {openEditCategoryModal(item)}}><Image src={pencil} alt="" className="w-4 h-4 cursor-pointer"/></button></td>
-                                                <td className="justify-center flex w-[25%] h-full border-b-2 p-2"><Image src={trash} alt="" className="w-4 h-4 cursor-pointer"/></td>
-                                            </tr>
-                                        </tbody>
-                                    )
-                                }
-                                )}
+                            {dataCategories.map((item) => {
+                                return (
+                                    <tbody className="overflow-y-auto">
+                                        <tr key={item.id} className="flex w-full h-14">
+                                            <td className="justify-center flex w-[25%] h-full border-b-2 p-2">{item.id}</td>
+                                            <td className="justify-start flex w-[25%] h-full border-b-2 p-2">{item.name}</td>
+                                            <td className="justify-center flex w-[25%] h-full border-b-2 p-2"><button onClick={() => { openEditCategoryModal(item) }}><Image src={pencil} alt="" className="w-4 h-4 cursor-pointer" /></button></td>
+                                            <td className="justify-center flex w-[25%] h-full border-b-2 p-2"><Image src={trash} alt="" className="w-4 h-4 cursor-pointer" /></td>
+                                        </tr>
+                                    </tbody>
+                                )
+                            }
+                            )}
                         </table>
                     </div>
 
-                    <div className={modalCategory ? "w-[50%] !disabled" : "disabled"}>
-                        <div className="p-6 flex flex-col w-96 fixed bg-opacity-50 z-50">
-                            <h2 className="text-xl font-semibold">Nova categoria</h2>
-                            <form className="flex flex-col">
-                                <label htmlFor="" className="mt-8">Nome</label>
-                                <input type="text" placeholder="Nome da categoria" value={name} onChange={(e) => { setName(e.target.value) }} className="border-2 rounded-[5px] p-1 mt-2 mb-10"></input>
-                            </form>
-                            <div className="flex justify-between">
-                                <button onClick={() => setModalCategory(false)} className="flex justify-center items-center h-8 text-[15px] bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">Cancelar</button>
-                                <button onClick={() => { addCategory() }} className="flex justify-center items-center h-8 text-[15px] bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">Adicionar categoria</button>
-                            </div>
-                        </div>
+                    <div className={(modalCategory || modalEditCategory) ? "w-[30%] !disabled" : "disabled"}>
+                            {modalCategory ? 
+                                <div className="p-6 flex flex-col w-96 fixed bg-opacity-50 z-50">
+                                    <h2 className="text-xl font-semibold">Nova categoria</h2>
+                                    <form className="flex flex-col">
+                                        <label htmlFor="" className="mt-8">Nome</label>
+                                        <input type="text" placeholder="Nome da categoria" value={name} onChange={(e) => { setName(e.target.value) }} className="border-2 rounded-[5px] p-1 mt-2 mb-10"></input>
+                                    </form>
+                                    <div className="flex justify-between">
+                                        <button onClick={() => setModalCategory(false)} className="flex justify-center items-center h-8 text-[15px] bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">Cancelar</button>
+                                        <button onClick={() => { addCategory() }} className="flex justify-center items-center h-8 text-[15px] bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">Adicionar categoria</button>
+                                    </div>
+                                </div>
+                            :
+                            modalEditCategory ?
+                                <div className="p-6 flex flex-col w-96 fixed bg-opacity-50 z-50">
+                                    <h2 className="text-xl font-semibold">Editar categoria</h2>
+                                    <form className="flex flex-col">
+                                        <label htmlFor="" className="mt-8">ID</label>
+                                        <input type="text" placeholder="Id da categoria" readOnly value={selectedCategory?.id} onChange={(e) => { setIdCategory(Number(e.target.value)) }} className="border-2 rounded-[5px] p-1 mt-2"></input>
+                                        <label htmlFor="" className="mt-4">Nome</label>
+                                        <input type="text" placeholder="Nome da categoria" value={selectedCategory?.name} onChange={(e) => { setEditName(e.target.value) }} className="border-2 rounded-[5px] p-1 mt-2 mb-10"></input>
+                                    </form>
+                                    <div className="flex justify-between">
+                                        <button onClick={() => setModalEditCategory(false)} className="flex justify-center items-center h-8 text-[15px] bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-600">Cancelar</button>
+                                        <button onClick={() => { updateCategory() }} className="flex justify-center items-center h-8 text-[15px] bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">Salvar alterações</button>
+                                    </div>
+                                </div>
+                            :
+                            <></>
+                            }
+                            
                     </div>
                 </div>
             </div>
