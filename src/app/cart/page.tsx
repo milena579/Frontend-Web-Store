ebb18d4132d219a5b03f754e04ddec04ba3476cd
milena579 <milecalegari@gmail.com>
2024-12-09 12:57:30 -0300

    mudando cores

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Menu } from '@/componets/menu/menu';
 import React, { useState } from "react";
 
 const Carrinho = () => {
@@ -38,53 +39,57 @@ const Carrinho = () => {
   };
 
   return (
-    <div className="max-w-3xl mx-auto p-6 bg-gray-100 rounded-xl">
-      <h2 className="text-center text-purple-700 mb-6 text-2xl">Meu Carrinho</h2>
-      
-      <div
-        className="flex justify-between p-4 bg-white mb-4 rounded-lg shadow-md"
-      >
-        <div>
-          <h3 className="text-lg font-semibold">Item</h3>
-          <p className="text-sm">Preço: R$</p>
-          <div className="flex items-center mt-3">
-            <button
-              className="px-4 py-2 bg-gray-300 rounded-md cursor-pointer mr-3"
-            >
-              -
-            </button>
-            <span className="text-lg font-semibold">1</span>
-            <button
-              className="px-4 py-2 bg-gray-300 rounded-md cursor-pointer ml-3"
-            >
-              +
-            </button>
-          </div>
+    <>
+        <Menu op1="" ></Menu>
+        <div className='items-center w-full h-screen justify-center'>
+            <div className="max-w-3xl mx-auto p-6 bg-gray-100 rounded-xl ">
+                <h2 className="text-center text-teal-500 mb-6 text-2xl">Meu Carrinho</h2>
+                
+                <div
+                    className="flex justify-between p-4 bg-white mb-4 rounded-lg shadow-md"
+                >
+                    <div>
+                    <h3 className="text-lg font-semibold">Item</h3>
+                    <p className="text-sm">Preço: R$</p>
+                    <div className="flex items-center mt-3">
+                        <button
+                        className="px-4 py-2 bg-gray-300 rounded-md cursor-pointer mr-3"
+                        >
+                        -
+                        </button>
+                        <span className="text-lg font-semibold">1</span>
+                        <button
+                        className="px-4 py-2 bg-gray-300 rounded-md cursor-pointer ml-3"
+                        >
+                        +
+                        </button>
+                    </div>
+                    </div>
+                    {/* <button onClick={() => abrirModal(produto.id)} className="text-red-600 border-none bg-transparent cursor-pointer">
+                    Remover
+                    </button> */}
+                </div>
+                
+                <div className="mt-6">
+                    <h4 className="text-lg font-medium">Subtotal: R$</h4>
+                    <h3 className="text-xl font-semibold">Total: R$</h3>
+                </div>
+                
+                <button
+                    className="w-full py-4 bg-teal-500 text-white text-lg font-semibold rounded-lg mt-6 cursor-pointer"
+                >
+                    Fechar Pedido
+                </button>
+                {/* <Modal
+                    isOpen={isModalOpen}
+                    onClose={fecharModal}
+                    onConfirm={confirmarRemocao}
+                    title="Confirmação"
+                    message="Tem certeza que deseja remover este produto?" 
+                /> */}
+            </div>
         </div>
-        {/* <button onClick={() => abrirModal(produto.id)} className="text-red-600 border-none bg-transparent cursor-pointer">
-          Remover
-        </button> */}
-      </div>
-     
-      <div className="mt-6">
-        <h4 className="text-lg font-medium">Subtotal: R$</h4>
-        <h4 className="text-lg font-medium">Entrega: R$</h4>
-        <h3 className="text-xl font-semibold">Total: R$</h3>
-      </div>
-      
-      <button
-        className="w-full py-4 bg-purple-700 text-white text-lg font-semibold rounded-lg mt-6 cursor-pointer"
-      >
-        Fechar Pedido
-      </button>
-      {/* <Modal
-        isOpen={isModalOpen}
-        onClose={fecharModal}
-        onConfirm={confirmarRemocao}
-        title="Confirmação"
-        message="Tem certeza que deseja remover este produto?" 
-      /> */}
-    </div>
+    </>
   );
 };
 
